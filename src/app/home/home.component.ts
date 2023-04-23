7dd6ce2df9ae4e00980ef960268a69aba36f3f67
Delsoc Ibrahim <dibrahim@cse-healthcare.io>
2023-04-23 18:51:50 +0200

    c

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  names: string[] = ['Delsoc Ibrahim', 'Softwareentwickler'];
+  currentName: string = '';
+  currentDots: string = '';
+
+  ngOnInit() {
+    this.animateText();
+  }
+
+  animateText() {
+    let index = 0;
+    setInterval(() => {
+      this.currentName = this.names[index];
+      if (index === 0) {
+        this.currentDots = '';
+        setTimeout(() => {
+          this.currentDots = '';
+        }, 1000);
+      } else if (index === 1) {
+        this.currentDots = '';
+        setTimeout(() => {
+          this.currentDots = '';
+        }, 1000);
+      }
+      index = (index === this.names.length - 1) ? 0 : index + 1;
+    }, 2000);
+  }
 
 }
